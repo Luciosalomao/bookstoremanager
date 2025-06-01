dc6cf447148b8eec462932f59945b9a94e64c21f
Lucio Marcos Salomão de Souza <lucio.souza@el.com.br>
2025-05-31 22:26:41 -0300

    Adicionando unit test para livro não encontrado

--- src/test/java/com/luciosalomao/bookstoremanager/services/BookServiceTest.java
+++ src/test/java/com/luciosalomao/bookstoremanager/services/BookServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.luciosalomao.bookstoremanager.entity.Books;
 
@@ -16,6 +15,8 @@ import java.util.Optional;
 
 import static com.luciosalomao.bookstoremanager.utils.BookUtils.createFakeBook;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,5 +37,15 @@ public class BookServiceTest {
         assertEquals(expectedFoundBook.getName(), bookDTO.getName());
         assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
         assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+
+    @Test
+    void whenGivenUnexistingIdThenNotFindThrowAnException() {
+        var invalidId = 10L;
+
+        when(bookRepository.findById(invalidId))
+                .thenReturn(Optional.ofNullable(any(Books.class)));
+
+        assertThrows(BookNotFoundException.class, () -> bookService.findById(invalidId));
     }
 }
